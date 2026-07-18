0525a9e7d87cd5b9258b9f902b76bdcaa752ccf8
André de Queiroz <andrepatrinicola@gmail.com>
2026-07-18 00:07:45 -0300

    adiciona sistema simples de meteoros

--- games/meteor/src/game/SpaceMeteor.ts
+++ games/meteor/src/game/SpaceMeteor.ts
@@ -7,32 +7,57 @@ import spaceShip from '../images/spaceship.png'
 export class SpaceMeteor extends Game {
 
     spaceShip: SpaceShip;
+    meteors: Meteor[];
+    meteorsInterval: number;
+    meteorsCounter: number;
+    eventsObserver: EventObserver;
 
     public constructor(canvas: HTMLCanvasElement) {
         super(canvas)
 
-        this.context.imageSmoothingEnabled = false;
+        this.eventsObserver = new EventObserver()
+        this.meteors = []
+        this.meteorsInterval = 80;
+        this.meteorsCounter = 0;
 
         this.spaceShip = new SpaceShip(
             this,
             canvas,
-            new Vector2({x: 0, y:0}),
-            new Vector2({x: 0, y:0}),
-            new Vector2({x: 10, y:10}),
+            new Vector2({ x: canvas.width / 2, y: canvas.height / 2 }),
+            new Vector2({ x: 0, y: 0 }),
+            new Vector2({ x: 75, y: 61 }),
             200
-        )
+            );
     }
 
     public update(dt: number): void {
         void dt;
 
         this.spaceShip.update(dt);
+
+
+        if (this.meteorsCounter >= this.meteorsInterval){
+            this.meteors.push(
+                new Meteor(this, this.canvas)
+            )
+            this.meteorsCounter = 0;
+        } else {
+            this.meteorsCounter += 1;
+        }
+
+        for (const mtr of this.meteors){
+            mtr.update(dt);
+        }
     }
 
     public render(): void {
         this.context.imageSmoothingEnabled = false;
         super.render()
         this.spaceShip.render();
+
+        for (const mtr of this.meteors){
+            mtr.render();
+        }
     }
 
 }
@@ -40,7 +65,7 @@ export class SpaceMeteor extends Game {
 
 export class SpaceShip {
 
-    public ctx: Game;
+    public ctx: SpaceMeteor;
     public position: Vector2;
     public velocity: Vector2;
     public dimension: Vector2;
@@ -57,7 +82,7 @@ export class SpaceShip {
     public sprite: HTMLImageElement;
 
     public constructor(
-        ctx: Game,
+        ctx: SpaceMeteor,
         canvas: HTMLCanvasElement, 
         position: Vector2, 
         velocity: Vector2, 
@@ -80,6 +105,9 @@ export class SpaceShip {
 
         this.sprite = new Image();
         this.sprite.src = spaceShip
+
+
+        this.ctx.eventsObserver.subscribe({key: 'shoot', event: this.shoot})
     }
 
     public update(dt: number): void {
@@ -98,11 +126,12 @@ export class SpaceShip {
         } 
 
         if(this.input.isKeyDown('enter') && !this.isLoading){
+            this.ctx.eventsObserver.notify('shoot')
             const missil = new Missil(
                 this.ctx, this.canvas, 
-                new Vector2({x: this.position.x, y:this.position.y}), 
+                new Vector2({x: this.position.x + this.sprite.naturalWidth / 2 , y:this.position.y}), 
                 new Vector2({x: 0, y: -1}), 
-                new Vector2({x:3 ,y: 3}), 500)
+                new Vector2({x:3 ,y: 3}), 500, 5)
             this.storage.push(missil);
             this.isLoading = true;
         }
@@ -138,6 +167,10 @@ export class SpaceShip {
             missile.render();
         }
     }
+
+    public shoot(){
+        console.log("Atirei")
+    }
 }
 
 
@@ -148,6 +181,7 @@ export class Missil {
     public velocity: Vector2;
     public dimension: Vector2;
     public speed: number;
+    public radius: number;
 
     public canvas: HTMLCanvasElement;
 
@@ -157,7 +191,8 @@ export class Missil {
         position: Vector2, 
         velocity: Vector2, 
         dimension: Vector2,
-        speed: number
+        speed: number,
+        radius: number
     ) {
         this.ctx = ctx;
         this.position = position;
@@ -165,6 +200,7 @@ export class Missil {
         this.dimension = dimension;
         this.speed = speed;
         this.canvas = canvas;
+        this.radius = radius;
     }
 
     public update(dt: number): void {
@@ -174,8 +210,110 @@ export class Missil {
 
     public render(): void {
         this.ctx.context.imageSmoothingEnabled = false;
-        this.ctx.context.fillRect(this.position.x, this.position.y, this.dimension.x, this.dimension.y);
+        this.ctx.context.beginPath();
+        this.ctx.context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, true);
+        this.ctx.context.closePath();
         this.ctx.context.fillStyle = 'black';
         this.ctx.context.fill();
+    }
+}
+
+
+
+export class Meteor {
+
+    public ctx: Game;
+    public position: Vector2;
+    public velocity: Vector2;
+    public dimension: Vector2;
+    public speed: number;
+    // public radius: number;
+    public rotation: number;
+
+    public canvas: HTMLCanvasElement;
+
+    public constructor(
+        ctx: Game,
+        canvas: HTMLCanvasElement, 
+
+    ) {
+        this.ctx = ctx;
+        this.canvas = canvas;
+
+        const getRandom = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+        this.position = new Vector2({
+            x: getRandom(0, this.ctx.canvas.width / 2),
+            y: -150
+        });
+
+        this.velocity = new Vector2({
+            x: getRandom(-10, 10), 
+            y: getRandom(30, 50)
+        });
+
+        this.dimension = new Vector2({
+            x:getRandom(40, 80),
+            y:getRandom(40, 80)
+        });
+
+        this.speed = getRandom(1, 2);
+        this.rotation = 35
+    }
+
+    public update(dt: number){
+        this.position.x += this.velocity.x * this.speed * dt;
+        this.position.y += this.velocity.y * this.speed * dt;
+
+
+        // this.rotation += 10 * dt;
+    }
+
+    public render(): void {
+        this.ctx.context.save();
+
+        this.ctx.context.translate(
+            this.position.x + this.dimension.x/2,
+            this.position.y + this.dimension.y/2
+        );
+        // this.ctx.context.rotate(this.rotation * Math.PI / 180);
+        this.ctx.context.imageSmoothingEnabled = false;
+        this.ctx.context.fillRect(this.position.x, this.position.y, this.dimension.x, this.dimension.y);
+        this.ctx.context.fillStyle = 'black';
+
+        this.ctx.context.restore();
+    }
+}
+
+
+type Event = {
+    key: string;
+    event: () => void;
+}
+
+class EventObserver {
+
+    events: Event[]
+
+    constructor(){
+        this.events = [];
+    }
+
+    public subscribe(event: Event){
+        console.log(event)
+        this.events.push(event);
+    }
+
+    // public unsubscribe(event: Event){
+    //     this.events.pop(event)
+    // }
+
+    public notify(key: string) {
+        for (const event of this.events){
+            if(event.key == key){
+                event.event();
+            }
+            // console.log(event)
+        }
     }
 }
